cf285abb07e33e5a94d144d920082104b1807412
chandrawiguna99 <chndrwgnn66@gmail.com>
2022-06-17 17:42:43 +0700

    mdnew

---
+++ config.js
@@ -0,0 +1,53 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'sonelstore',
+}
+
+// Other
+global.owner = ['6285861009691']
+global.premium = ['6285861009691']
+global.namaowner = ['ChandraPedia']
+global.namabot = ['Bot']
+global.packname = 'Punya'
+global.author = 'Chandra'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success..... _JANGAN LUPA FOLLOW IG OWNER_ _*@chndrwgnn*_',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: '*Prosessss kack,* _FOLLOW INSTAGRAM OWNER_ *_@chndrwgnn__*',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
